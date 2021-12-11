3fbcd7dffe053b6d1f3fa0fdae94e4314f5226b4
장 세영(Jang Seyoung) <92082361+hotsauceuniverse@users.noreply.github.com>
2021-12-11 23:51:57 +0900

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,36 @@
+<template>
+  <button @click="handler('hi', $event)">
+   click 1!
+  </button>
+  <button @click="handler('hello', $event)">
+   click 2!
+  </button><br>
+  <a href="https://www.naver.com" target="_blank" @click.prevent="naver">Naver</a>
+  
+  <h2>{{ message }}</h2>
+  <input type="text" :value="message" @input="message = $event.target.value">
+  <h2>{{ checked }}</h2>
+  <input type="checkbox" v-model="checked">
+
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      message: 'Hello World',
+      checked: false
+    }
+  },
+
+  methods: {
+    handler(msg, event) {
+      console.log(msg)
+      console.log(event)
+    },
+    naver() {
+      console.log('Naver')
+    }
+  }
+}
+</script>
